da2b65d747d74355b16d63ba8586b985902b3c6f
Javier Aviles <avileslopez.javier@gmail.com>
2018-05-13 20:05:50 +0200

    Logging errors in error.log file. Winston config into logging.ts

--- src/logging.ts
+++ src/logging.ts
@@ -1,6 +1,6 @@
 import * as Koa from 'koa';
 import { config } from './config';
-
+import * as winston from 'winston';
 
 export function logger(winstonInstance) {
     return async(ctx: Koa.Context, next: () => Promise<any>) => {
@@ -23,6 +23,18 @@ export function logger(winstonInstance) {
         }
 
         const msg: string = `${ctx.method} ${ctx.originalUrl} ${ctx.status} ${ms}ms`;
+
+        winstonInstance.configure({
+            level: 'debug',
+            transports: [
+                //
+                // - Write to all logs with level `debug` and below to console.
+                new winston.transports.Console({ colorize: true }),
+                //
+                // - Write all logs error (and below) to `error.log`.
+                new winston.transports.File({ filename: 'error.log', level: 'error' })
+            ]
+        });
 
         winstonInstance.log(logLevel, msg);
     };
--- src/server.ts
+++ src/server.ts
@@ -2,23 +2,14 @@ import * as Koa from 'koa';
 import * as winston from 'winston';
 import { logger } from './logging';
 
-winston.configure({
-    level: 'debug',
-    transports: [
-        new winston.transports.Console({
-            colorize: true
-        })
-    ]
-});
-
 import { config } from './config';
 import { routes } from './routes';
 
 const app = new Koa();
 
-app.use(routes);
-
 app.use(logger(winston));
+
+app.use(routes);
 
 app.listen(config.port);
 
