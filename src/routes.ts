820066193c16d76f71c391eb72443fa7459308a1
Javier Aviles <avileslopez.javier@gmail.com>
2018-05-23 15:51:29 +0200

    DB connection, user entity and simple crud working

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import * as Router from 'koa-router';
+import { getManager } from 'typeorm';
+import { User } from './entity/user';
 
 const router = new Router();
 
@@ -11,6 +13,43 @@ router.get('/jwt', async (ctx) => {
     ctx.status = 201;
     // the body of the response will contain the information contained as payload in the JWT
     ctx.body = ctx.state.user;
+});
+
+router.get('/users', async (ctx) => {
+
+    // get a user repository to perform operations with user
+    const userRepository = getManager().getRepository(User);
+
+    // load all users
+    const users = await userRepository.find();
+
+    // return loaded users
+    ctx.body = users;
+});
+
+router.post('/user', async (ctx) => {
+
+    // get a user repository to perform operations with user
+    const userRepository = getManager().getRepository(User);
+
+    // save the user contained in the POST body
+    const user = await userRepository.save(ctx.request.body);
+
+    // return the created user
+    ctx.body = user;
+});
+
+router.delete('/user/:id', async (ctx) => {
+
+    // get a user repository to perform operations with user
+    const userRepository = getManager().getRepository(User);
+
+    // find and remove the user by specified id
+    const userToRemove = await userRepository.findOne(ctx.params.id);
+    await userRepository.delete(userToRemove);
+
+    // return a NO CONTENT status code
+    ctx.status = 204;
 });
 
 export const routes = router.routes();
