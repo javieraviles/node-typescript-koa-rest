8719ac7cf321d66fdc8511e81a6146c6ea2656dc
Aviles, Javier <franciscojavier.avileslopez@nttdata.com>
2018-05-23 15:51:29 +0200

    DB connection, user entity and simple crud working

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,38 @@
 import * as Koa from 'koa';
 import * as jwt from 'koa-jwt';
+import * as bodyParser from 'koa-bodyparser';
 import * as winston from 'winston';
 import * as dotenv from 'dotenv';
+import { createConnection } from 'typeorm';
+import 'reflect-metadata';
 
+import { User } from './entity/user';
 import { logger } from './logging';
 import { config } from './config';
 import { routes } from './routes';
 
-const app = new Koa();
+// create connection with database
+// note that its not active database connection
+// TypeORM creates you connection pull to uses connections from pull on your requests
+createConnection().then(async connection => {
 
-// Load environment variables from .env file, where API keys and passwords are configured
-dotenv.config({ path: '.env' });
+    const app = new Koa();
 
-// Logger middleware -> use winston as logger (logging.ts with config)
-app.use(logger(winston));
+    // Load environment variables from .env file, where API keys and passwords are configured
+    dotenv.config({ path: '.env' });
 
-// JWT middleware -> below this line routes are only reached if JWT token is valid, secret as env variable
-app.use(jwt({ secret: process.env.JWT_SECRET }));
+    // Logger middleware -> use winston as logger (logging.ts with config)
+    app.use(logger(winston));
 
-app.use(routes);
+    app.use(bodyParser());
 
-app.listen(config.port);
+    // JWT middleware -> below this line routes are only reached if JWT token is valid, secret as env variable
+    app.use(jwt({ secret: process.env.JWT_SECRET }));
 
-console.log(`Server running on port ${config.port}`);
+    app.use(routes);
+
+    app.listen(config.port);
+
+    console.log(`Server running on port ${config.port}`);
+
+}).catch(error => console.log('TypeORM connection error: ', error));
