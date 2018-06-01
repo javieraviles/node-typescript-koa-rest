32fee7270d4f69cf3aac262f97a996a6c4c1f0d2
Javier Aviles <avileslopez.javier@gmail.com>
2018-06-01 19:59:13 +0200

    Back ignoring dist

--- dist/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    port: process.env.NODE_PORT || 3000,
-    debugLogging: process.env.NODE_ENV == 'development',
-};
-exports.config = config;
-//# sourceMappingURL=config.js.map
