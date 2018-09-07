fa11c9495da357c750b61641c4ceb065b6e6a8c4
Aviles, Javier <franciscojavier.avileslopez@nttdata.com>
2018-09-07 14:21:47 +0200

    Fixes for 1.4.1

--- src/config.ts
+++ src/config.ts
@@ -5,13 +5,13 @@ dotenv.config({ path: '.env' });
 export interface IConfig {
     port: number;
     debugLogging: boolean;
-    DbSslConn: boolean;
+    dbsslconn: boolean;
     jwtSecret: string;
     databaseUrl: string;
 }
 
-const config = {
-    port: process.env.PORT || 3000,
+const config: IConfig = {
+    port: +process.env.PORT || 3000,
     debugLogging: process.env.NODE_ENV == 'development',
     dbsslconn: process.env.NODE_ENV != 'development',
     jwtSecret: process.env.JWT_SECRET || 'your-secret-whatever',
